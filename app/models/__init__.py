7d5093b7fb447173ee5df80f3e1a12248900da1f
despachanetbo-coder <despachanet.bo@gmail.com>
2025-12-27 14:11:50 -0400

    actualizacion de constructor de capeta models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,26 @@
-# app/models/__init__.py
+# app/models/__init__.py - VERSIÓN CORREGIDA
+from .base_model import BaseModel
+from .auditoria_transacciones_model import AuditoriaTransaccionesModel
+from .comprobantes_adjuntos_model import ComprobantesAdjuntosModel
+from .configuracion_model import ConfiguracionesModel
+from .cuota_model import CuotaModel
+from .estudiante_model import EstudianteModel
+from .docente_model import DocenteModel
+from .usuarios_model import UsuariosModel
+from .programa_academico_model import ProgramasAcademicosModel as ProgramaAcademicoModel
 
-from base_model import BaseModel
-from auditoria_transacciones_model import AuditoriaTransaccionesModel
-from comprobantes_adjuntos_model import ComprobantesAdjuntosModel
-from configuracion_model import ConfiguracionesModel
-from cuota_model import CuotaModel
+# Para compatibilidad, también podemos exportarlo con ambos nombres
+ProgramasAcademicosModel = ProgramaAcademicoModel
+
+# Lista de todos los modelos para fácil importación
+__all__ = [
+    "BaseModel",
+    "ProgramaAcademicoModel",
+    "EstudianteModel",
+    "DocenteModel",
+    "UsuariosModel",
+    "AuditoriaTransaccionesModel",
+    "ComprobantesAdjuntosModel",
+    "ConfiguracionesModel",
+    "CuotaModel",
+]
